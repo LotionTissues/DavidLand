6e7752becf85f51915c9115304ea0704c2ae2dd5
LotionTissues <113185392+LotionTissues@users.noreply.github.com>
2023-03-25 14:47:20 -0400

    cool
    yes

--- Assets/Screpts/HawgRidaa.cs
+++ Assets/Screpts/HawgRidaa.cs
@@ -4,24 +4,77 @@ using UnityEngine;
 
 public class HawgRidaa : MonoBehaviour
 {
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    public ParticleSystem smokeEffect;
+    
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    bool broken = true;
+    
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        float horizontal2 = Input.GetAxis("horizontal2");
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal2;
-
-
-        float vertical2 = Input.GetAxis("vertical2");
-
-       
-        position.y = position.y + 0.1f * vertical2;
-        transform.position = position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
         
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
+        Vector2 position = rigidbody2D.position;
+        
+        
+        
+        rigidbody2D.MovePosition(position);
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        FurryController player = other.gameObject.GetComponent<FurryController >();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+    
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+        
+        smokeEffect.Stop();
     }
 }
